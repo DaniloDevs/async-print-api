3f688f039e926d01f66c07cb5d45f8fde1c99576
Danilo Romão <https.sw@gmail.com>
2025-12-01 14:19:53 -0300

    Refactor: templete text works

--- src/jobs/worker.ts
+++ src/jobs/worker.ts
@@ -3,45 +3,41 @@ import { env } from "../env";
 import { getPrinter } from "../utils/setup-printer";
 import type { Lead } from "../types/lead";
 import path from "path";
+import type { ThermalPrinter } from "node-thermal-printer";
 
 
+async function TempleteTickt(printer: ThermalPrinter, job: Job<Lead>) {
+   // Titulo 
+   printer.alignCenter();
+   printer.setTextNormal()
+   printer.bold(true);
+   printer.println("Ticket do Sorteio");
+   printer.newLine();
 
+   // Inserindo dados
+   printer.alignLeft();
+   printer.bold(false);
+   printer.print("Participante: ")
+   printer.bold(true);
+   printer.println(job.data.name)
+   printer.bold(false);
+   printer.print("Final do Telefone: ")
+   printer.bold(true);
+   printer.println(job.data.cellphone.slice(-4))
+
+   // Footer
+   printer.newLine();
+   printer.alignRight();
+   printer.println(new Date().toLocaleString('pt-BR'))
+   printer.cut()
+}
 
 new Worker(
    "leads-enqueue",
    async (job: Job<Lead>) => {
       const printer = getPrinter();
-      
-      
-      const imagePath = path.resolve(process.cwd(), "src/assets/fatos-fotos.png");
-      printer.alignCenter();
-      
-      await printer.printImage(imagePath);
 
-
-      // Titulo 
-      printer.alignCenter();
-      printer.setTextNormal()
-      printer.bold(true);
-      printer.println("Ticket do Sorteio");
-      printer.newLine();
-
-      // Inserindo dados
-      printer.alignLeft();
-      printer.bold(false);
-      printer.print("Partipante: ")
-      printer.bold(true);
-      printer.println(job.data.name)
-      printer.bold(false);
-      printer.print("Final do Telefone: ")
-      printer.bold(true);
-      printer.println(job.data.cellphone.slice(-4))
-
-      // Footer
-      printer.newLine();
-      printer.alignRight();
-      printer.println(new Date().toLocaleString('pt-BR'))
-      printer.cut()
+      await TempleteTickt(printer,job)
 
       await printer.execute();
       console.log("Executed Job")
